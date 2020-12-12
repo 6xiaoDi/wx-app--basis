e2b4c727995b9a80a1b0801cec5d5e91671308d8
6xd <ldtheended@163.com>
2020-12-12 14:21:04 +0800

    a1.45（生命周期函数执行顺序）

--- pages/news/news.js
+++ pages/news/news.js
@@ -5,32 +5,28 @@ Page({
      * 页面的初始数据
      */
     data: {
-        a:"2020LPL夏季赛季后赛观赛指南",
-        b:"0000",
-        c:"0000"
+        a:"2020LPL夏季赛季后赛观赛指南"
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        this.setData({
-            b:"2021LPL"
-        })
+        console.log('onLoad');
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        console.log(this.data);
+        console.log('onReady');
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        console.log('onShow');
     },
 
     /**
@@ -44,7 +40,7 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        console.log('onUnload');
     },
 
     /**
