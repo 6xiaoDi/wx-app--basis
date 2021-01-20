5fc67215c04a3715dc71550718a44d24e8bfbee9
6xd <ldtheended@163.com>
2021-01-20 14:23:04 +0800

    a2.43（配置标题与动态配置标题——动态配置标题）

--- pages/more-movies/more-movies.js
+++ pages/more-movies/more-movies.js
@@ -34,7 +34,21 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+      let title = '电影'
+      switch(this.data._type){
+        case 'in_theaters':
+          title='正在热映'
+          break
+        case 'coming_soon':
+          title = '即将上映'
+          break
+        case 'top250':
+          title = '豆瓣Top250'
+          break
+      }
+      wx.setNavigationBarTitle({
+        title: title,
+      })
     },
 
     /**
