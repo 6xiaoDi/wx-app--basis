6a3ff6746610d13b405cbf839a9211408160084c
6xd <ldtheended@163.com>
2021-01-20 17:55:02 +0800

    a2.51（电影详情页面——从服务端获取数据）

--- pages/movie-detail/movie-detail.js
+++ pages/movie-detail/movie-detail.js
@@ -1,4 +1,5 @@
 // pages/movie-detail/movie-detail.js
+const app = getApp()
 Page({
 
     /**
@@ -12,7 +13,13 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const mid = options.mid
+        wx.request({
+            url: app.gBaseUrl + 'subject/' + mid,
+            success:(res)=>{
+              console.log(res.data)
+            }
+          })
     },
 
     /**
